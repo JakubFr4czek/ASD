a389c9f275b654ea62978d09ebf3f963ea36e5ca
JakubFr4czek <jakubfraczek@student.agh.edu.pl>
2023-04-13 09:05:34 +0200

    Rework

--- kolosy_asd_2022/kol2_a_dynamic/kol2a.py
+++
@@ -1,8 +0,0 @@
-from kol2atesty import runtests
-
-def drivers( P, B ):
-    # tu prosze wpisac wlasna implementacje
-    return []
-
-# zmien all_tests na True zeby uruchomic wszystkie testy
-runtests( drivers, all_tests = False )
