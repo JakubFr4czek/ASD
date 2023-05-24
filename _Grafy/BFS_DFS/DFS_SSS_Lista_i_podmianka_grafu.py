e41bc7d09d2450a1cb7c614fe2c54cbe25116220
JakubFr4czek <jakubfraczek@student.agh.edu.pl>
2023-05-24 12:47:54 +0200

    I was acting

--- _Grafy/BFS_DFS/DFS_SSS_Lista_i_podmianka_grafu.py
+++ _Grafy/BFS_DFS/DFS_SSS_Lista_i_podmianka_grafu.py
@@ -1,7 +1,3 @@
-from collections import deque
-#Listowo: O(E + V)
-#Krawedzie odwiedzone w kazdym dfsvisit, tworza silnie spojna skladowa
-
 def DFS_first(G): #Pierwszy, zwykly DFS
 
     def DFSVisit(G, v):
@@ -70,41 +66,6 @@ def reverseEdges( G ): #Listowo
             H[G[i][j]].append(i)
 
     return H
-
-def DFS_third(G): #topological sort
-
-    def DFSVisit(G, v):
-
-        nonlocal time
-        time += 1
-
-        visited[v] = True
-
-        #print("v: ",v," czas: ", time)
-
-        for i in range(len(G[v])):
-
-            if visited[G[v][i]] == False:
-                path[G[v][i]] = v
-                DFSVisit(G,G[v][i])
-
-        time += 1 #Tak jakos lepiej to liczy jak jest zakomentowane
-        topological.append(v)
-
-
-    visited = [False for _ in range(len(G))]
-    path = [-1 for _ in range(len(G))]
-    topological = deque()
-
-    time = 0 #czas przetworzenia wierzcholka
-
-    for i in range(len(G)):
-
-        if visited[i] == False:
-            DFSVisit(G, i)
-
-    return topological
-
 def SilnieSpojneSkladowe( G ):
 
     order = [i for i in range(len(G))]
@@ -120,6 +81,8 @@ def SilnieSpojneSkladowe( G ):
 
     silnieSpojneSkladowe, id = DFS_second(H, order)
 
+    print(silnieSpojneSkladowe)
+
     G2 = [ [] for _ in range(id) ]
 
     for i in range(len(G)):
@@ -134,45 +97,5 @@ def SilnieSpojneSkladowe( G ):
             if v1 != v2:
 
                 G2[v1].append(v2)
-
-
-    topological = DFS_third(G2) #topological od tylu
-
-
-    eval = [False for i in range(len(topological))]
-
-    #Liczymy sasiadow, i zmieniamy ich na true, jesli napotkamy falsz to drodze to chuj
-    #A jak nie => istnieje sobry początek
-
-    eval[topological(len(topological) - 1)] = True
-
-    for i in range(len(topological) -1, -1, -1):
-
-        if eval[topological[i]] == False:
-            return False
-        else:
-            for j in range(len(G2[topological[i]])):
-                eval[G2[topological[i]][j]] = True
-    #Moze nie dzialac, ale cos takiego
-
-        
-    
-
-
-G = [
-
-    [1,3],
-    [2,10],
-    [3],
-    [4],
-    [2,5],
-    [2],
-    [5,9],
-    [6],
-    [7],
-    [8],
-    [0,9]
-
-]
-
-SilnieSpojneSkladowe(G)
+    for i in range(len(G2)):
+        print(G2[i])
